eba4c6991a74fa17d90c7bf38ca4002084abc5ab
Carter Klein <carterklein13@gmail.com>
2020-02-14 14:22:41 -0500

    Lay out basic Google OAuth and custom deploy script skeleton

---
+++ cmd/deploy.js
@@ -0,0 +1,4 @@
+console.log('testing deployment...')
+
+// TODO add deployment steps to deploy to Heroku here.
+// TODO user just runs `npm run deploy` and app is deployed to Heroku
--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import GoogleLogin from 'react-google-login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let isLoggedIn = false
+
+const responseGoogle = (response) => {
+  isLoggedIn = true
+  console.log(response)
+  return App()
 }
 
-export default App;
+function App() {
+  console.log(isLoggedIn)
+  if (isLoggedIn) {
+    console.log('Logged in!')
+  } else {
+    console.log('Not logged in!')
+    return (
+      <div className="App">
+        <GoogleLogin
+          clientId="875281779878-d9i5m0kn6uehajcocuepfm568ov6hspa.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+      </div>
+    )
+  }
+}
+
+export default App
